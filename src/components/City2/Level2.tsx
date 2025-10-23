9e4fa9cd55eb3b1dc7c47efda98ebcfdfb5fb627
matthewpurkiss <mathewpurkiss04@gmail.com>
2025-10-23 17:56:31 +0200

    level 2 roads

--- src/components/City2/Level2.tsx
+++ src/components/City2/Level2.tsx
@@ -1,10 +1,16 @@
 import type { JSX } from "react/jsx-runtime";
 import { RoadLevel2 } from "./Road/RoadLevel2";
+import { BuildingsLevel2 } from "./Buildings/BuildingsLevel2";
 export default function Level2(props: JSX.IntrinsicElements["group"]) {
   return (
     <group {...props}>
         <RoadLevel2
         position={[100, 0.1, -150]}
+        scale={0.4}
+        rotation={[0, 0, 0]}/>
+
+        <BuildingsLevel2
+        position={[259.5, 0.1, -120]}
         scale={0.4}
         rotation={[0, 0, 0]}/>
       
