2aec666b2128c6e537fe586fb8b52070d2082f78
yabedea <yabsira.gebremichael11@gmail.com>
2025-10-30 16:39:57 +0200

    waypoints for city2 and city3

--- src/components/City3/NewRoadtest.tsx
+++ src/components/City3/NewRoadtest.tsx
@@ -1,13 +1,20 @@
+import { useMemo } from "react";
 import { useGLTF, Clone } from "@react-three/drei";
 import type { JSX } from "react/jsx-runtime";
+import { tagRoadSurfaces } from "../../utils/markRoadSurfaces";
 
 export function NewRoadtest(props: JSX.IntrinsicElements["group"]) {
-  const { scene } = useGLTF("models/City3RoadsFixed.glb");
+  const { scene } = useGLTF("/models/City3RoadsFixed.glb");
+  const preparedScene = useMemo(() => {
+    tagRoadSurfaces(scene, { includeNameHints: ["road"] });
+    return scene;
+  }, [scene]);
+
   return (
     <group {...props}>
-      <Clone object={scene} />
+      <Clone object={preparedScene} />
     </group>
   );
 }
 
-useGLTF.preload("models/City3RoadsFixed.glb");
+useGLTF.preload("/models/City3RoadsFixed.glb");
