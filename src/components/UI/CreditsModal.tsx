43affa562b87e0bfac833155248550c4872eb5b0
Kamal Lalloo <kamal.lalloo10@gmail.com>
2025-10-31 23:21:37 +0200

    added models used

--- src/components/UI/CreditsModal.tsx
+++ src/components/UI/CreditsModal.tsx
@@ -123,109 +123,172 @@ const CreditsModal: FC<CreditsModalProps> = ({ onClose }) => {
               fontSize: "0.95rem",
             }}
           >
-            <p>
-              • Adventurer — Quaternius
-              <br />
-              <a
-                href="https://poly.pizza/m/5EGWBMpuXq"
-                target="_blank"
-                rel="noopener noreferrer"
-                style={{ color: "#ffcc00", textDecoration: "none" }}
-              >
-                https://poly.pizza/m/5EGWBMpuXq
-              </a>
-            </p>
-
-            <p>
-              • Animated Woman — Quaternius
-              <br />
-              <a
-                href="https://poly.pizza/m/9kF7eTDbhO"
-                target="_blank"
-                rel="noopener noreferrer"
-                style={{ color: "#ffcc00", textDecoration: "none" }}
-              >
-                https://poly.pizza/m/9kF7eTDbhO
-              </a>
-            </p>
-
-            <p>
-              • Barrier Large — Quaternius
-              <br />
-              <a
-                href="https://poly.pizza/m/gLbBiYwt7l"
-                target="_blank"
-                rel="noopener noreferrer"
-                style={{ color: "#ffcc00", textDecoration: "none" }}
-              >
-                https://poly.pizza/m/gLbBiYwt7l
-              </a>
-            </p>
-
-            <p>
-              • Basketball court — Poly by Google
-              <br />
-              <a
-                href="https://poly.pizza/m/85FpUr2fHkH"
-                target="_blank"
-                rel="noopener noreferrer"
-                style={{ color: "#ffcc00", textDecoration: "none" }}
-              >
-                https://poly.pizza/m/85FpUr2fHkH
-              </a>
-            </p>
-
-            <p>
-              • Bench — Ev Amitay
-              <br />
-              <a
-                href="https://poly.pizza/m/dOSjmdmKaxi"
-                target="_blank"
-                rel="noopener noreferrer"
-                style={{ color: "#ffcc00", textDecoration: "none" }}
-              >
-                https://poly.pizza/m/dOSjmdmKaxi
-              </a>
-            </p>
-
-            <p>
-              • Bicycle — Poly by Google
-              <br />
-              <a
-                href="https://poly.pizza/m/19VoUuA2pcN"
-                target="_blank"
-                rel="noopener noreferrer"
-                style={{ color: "#ffcc00", textDecoration: "none" }}
-              >
-                https://poly.pizza/m/19VoUuA2pcN
-              </a>
-            </p>
-
-            <p>
-              • Big Building — Quaternius
-              <br />
-              <a
-                href="https://poly.pizza/m/AVCS8jUd2l"
-                target="_blank"
-                rel="noopener noreferrer"
-                style={{ color: "#ffcc00", textDecoration: "none" }}
-              >
-                https://poly.pizza/m/AVCS8jUd2l
-              </a>
-            </p>
-
-            <p>
-              • Billboard — Poly by Google
-              <br />
-              <a
-                href="https://poly.pizza/m/e5jmUgocmaT"
-                target="_blank"
-                rel="noopener noreferrer"
-                style={{ color: "#ffcc00", textDecoration: "none" }}
-              >
-                https://poly.pizza/m/e5jmUgocmaT
-              </a>
-            </p>
+            {[
+              ["Adventurer", "https://poly.pizza/m/5EGWBMpuXq", "Quaternius"],
+              [
+                "Animated Woman",
+                "https://poly.pizza/m/9kF7eTDbhO",
+                "Quaternius",
+              ],
+              [
+                "Barrier Large",
+                "https://poly.pizza/m/gLbBiYwt7l",
+                "Quaternius",
+              ],
+              [
+                "Basketball court",
+                "https://poly.pizza/m/85FpUr2fHkH",
+                "Poly by Google",
+              ],
+              ["Bench", "https://poly.pizza/m/dOSjmdmKaxi", "Ev Amitay"],
+              ["Bicycle", "https://poly.pizza/m/19VoUuA2pcN", "Poly by Google"],
+              ["Big Building", "https://poly.pizza/m/AVCS8jUd2l", "Quaternius"],
+              [
+                "Billboard",
+                "https://poly.pizza/m/e5jmUgocmaT",
+                "Poly by Google",
+              ],
+              ["Box", "https://poly.pizza/m/HvjissDrdr", "Kenny"],
+              ["Brits House", "https://poly.pizza/m/2K3bGB-w2qa", "Anonymous"],
+              [
+                "Brown Building",
+                "https://poly.pizza/m/fGKIlWGDNH",
+                "J-Toastie",
+              ],
+              [
+                "Building Green",
+                "https://poly.pizza/m/FmjsfA1eHY",
+                "J-Toastie",
+              ],
+              [
+                "Building Red Corner",
+                "https://poly.pizza/m/9JuFwnivP0",
+                "J-Toastie",
+              ],
+              ["Building Red", "https://poly.pizza/m/lbNz2dClar", "J-Toastie"],
+              ["Bush", "https://poly.pizza/m/d6STyhH76Qe", "Jarlan Perez"],
+              ["Camaro ZL1", "https://poly.pizza/m/7bF7UVAoYRG", "Kris Tong"],
+              ["Cone", "https://poly.pizza/m/WoXpAJT0oD", "J-Toastie"],
+              [
+                "Convertible",
+                "https://poly.pizza/m/dggOiBLYyuR",
+                "Poly by Google",
+              ],
+              [
+                "Debris Papers",
+                "https://poly.pizza/m/MujITy1NRR",
+                "Quaternius",
+              ],
+              ["Dumpster", "https://poly.pizza/m/PKsbolkZSr", "Quaternius"],
+              [
+                "Farm house",
+                "https://poly.pizza/m/bHyQe5jzdiQ",
+                "Poly by Google",
+              ],
+              ["Fence", "https://poly.pizza/m/ZSQMyIqPTz", "J-Toastie"],
+              ["Ferrari F40", "https://poly.pizza/m/RTwim9bhNd", "PuKkBuMXDD"],
+              [
+                "Fire hydrant",
+                "https://poly.pizza/m/eNPaSEPrst8",
+                "Poly by Google",
+              ],
+              ["Floor Hole", "https://poly.pizza/m/FbJAtOQ8pb", "J-Toastie"],
+              ["Flower Pot", "https://poly.pizza/m/u0CrZnpykb", "Zsky"],
+              ["Gazebo", "https://poly.pizza/m/b_p530Qy_-t", "Poly by Google"],
+              ["Glass", "https://poly.pizza/m/3v7i0dz7Vg", "MilkandBanana"],
+              [
+                "Grass Tile",
+                "https://poly.pizza/m/achm-Cr9Rr3",
+                "Adam Tomkins",
+              ],
+              ["Hedge", "https://poly.pizza/m/So5EAOMoNy", "Isa Lousberg"],
+              [
+                "Hooded Figure",
+                "https://poly.pizza/m/0domsLgwnKq",
+                "Thomas DR",
+              ],
+              [
+                "Hospital",
+                "https://poly.pizza/m/asNvyjkcSG1",
+                "Poly by Google",
+              ],
+              [
+                "House with driveway",
+                "https://poly.pizza/m/bnZkUs4qEdG",
+                "Poly by Google",
+              ],
+              ["House", "https://poly.pizza/m/roqiHdrpgc", "Quaternius"],
+              [
+                "Lamborghini",
+                "https://poly.pizza/m/5zUWP5UsLg-",
+                "Ignition Labs",
+              ],
+              ["Mailbox", "https://poly.pizza/m/9gbHlg1IlY", "J-Toastie"],
+              [
+                "Man hole cover",
+                "https://poly.pizza/m/S6aGVNF1Iz",
+                "J-Toastie",
+              ],
+              [
+                "Nissan GTR",
+                "https://poly.pizza/m/a_HKCtYAv2W",
+                "David Sirera",
+              ],
+              [
+                "Path Straight",
+                "https://poly.pizza/m/ZuRHRsKWoz",
+                "Quaternius",
+              ],
+              [
+                "Planter and Bushes",
+                "https://poly.pizza/m/wn85Xr9lM3",
+                "J-Toastie",
+              ],
+              [
+                "Play structure",
+                "https://poly.pizza/m/ee0cso-KZnC",
+                "Emmett “TawpShelf” Baber",
+              ],
+              ["Power Box", "https://poly.pizza/m/WFVjj4vnGg", "J-Toastie"],
+              [
+                "Rolls Royce",
+                "https://poly.pizza/m/3DtJTlxgO_U",
+                "David Sirera",
+              ],
+              ["Slide", "https://poly.pizza/m/dDe3njWPbg0", "Poly by Google"],
+              [
+                "Stop sign",
+                "https://poly.pizza/m/60GyU9CdZ9r",
+                "Poly by Google",
+              ],
+              ["Streetlight", "https://poly.pizza/m/nFwrlcLvM5", "Quaternius"],
+              [
+                "Swing set",
+                "https://poly.pizza/m/e-IJdcqZH4p",
+                "Poly by Google",
+              ],
+              ["Taxi", "https://poly.pizza/m/x43lOScTpN", "Quaternius"],
+              [
+                "Tennis court",
+                "https://poly.pizza/m/7cVH1Xt-LYy",
+                "Poly by Google",
+              ],
+              ["Blind woman", "https://poly.pizza/m/qJ2gsTUBHL", "Quaternius"],
+              ["Tree", "https://poly.pizza/m/cRipmFHCEVU", "Marc Solà"],
+            ].map(([name, link, author]) => (
+              <p key={name}>
+                • {name} — {author}
+                <br />
+                <a
+                  href={link}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  style={{ color: "#ffcc00", textDecoration: "none" }}
+                >
+                  {link}
+                </a>
+              </p>
+            ))}
           </div>
 
           <p
